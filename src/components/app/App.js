d10b59609dc27fd376dd371a02d710efa25a2c4b
Yakov <yakov@hnode1.example.com>
2021-02-25 09:17:45 +0300

    exist-simple-logic

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,11 +5,56 @@ import './App.css';
 
 import PlayingField from '../playing-field';
 
+const isInRange = (num, min, max) => {
+        return num >= min && num < max;
+    }
+const getReverseColor = (color) => {
+    if (color==="black")
+        return "white";
+    if (color==="white")
+        return "black";
+
+}
+
+const getResInDir = (arr, curr_color, idx1, idx2, step_i, step_j) => { // res in some direction
+    let res = 0;
+    let i = idx1 + step_i, j = idx2 + step_j;
+
+    while(true){
+        if(!isInRange(i,0,8) || !isInRange(j,0,8) || arr[i][j].status==="empty")
+            return 0;
+
+        if(arr[i][j].status===curr_color)
+            return res;
+        if(arr[i][j].status===getReverseColor(curr_color)){
+            res++;
+            i = i + step_i;
+            j = j + step_j;
+        }
+    }
+}
+
+const getResInAllDir = (arr, curr_color, idx1, idx2) => {
+    let result = 0;
+    //result += getResInDir(arr, curr_color, idx1, idx2, )
+    //result += getResInDir(arr, curr_color, idx1, idx2, )
+    for(let step_i = -1; step_i<=1; step_i++)
+        for(let step_j = -1; step_j<=1; step_j++)
+        if(! (step_i===0 && step_j===0)){
+            console.log(getResInDir(arr, curr_color, idx1, idx2, step_i, step_j));
+            result += getResInDir(arr, curr_color, idx1, idx2, step_i, step_j);
+        }
+
+}
 
 
 export default class App extends React.Component{
 
     state = {};
+
+    
+
+    
 
     constructor(props) {
         super(props);
@@ -20,7 +65,7 @@ export default class App extends React.Component{
 
         for (let i = 0; i < 8; i++)
             for (let j = 0; j < 8; j++) {
-                this.state.board[i][j] = {id: String(10 * i + j), status: 'black'};
+                this.state.board[i][j] = {id: String(10 * i + j), status: 'empty'};
             }
         console.log(this.state.board)
     }
